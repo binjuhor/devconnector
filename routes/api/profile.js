7750cf08f9a307fcb120662c86fa8ed56dbbc95c
Hoang Kiem <kiemhd@outlook.com>
2020-02-14 02:09:31 +0700

    Work with database Create user Token jwt user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-//@route  GET api/profile
-//@desc   Test route
-//@access Public
-router.get('/', (req, res)=>res.send('Profile route'));
+//@route  GET api/profile/me
+//@desc   Get current users profile
+//@access Private
+router.post('/me', auth, async (req, res)=>{
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            'user',
+            ['name', 'avatar']
+        );
+    } catch (err) {
+        console.error(err.message);
+        re.status(500).send('Server Error');
+    }
+});
 module.exports = router;
