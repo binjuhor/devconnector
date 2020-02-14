5b3f1602c840f000931ce58aa01f2c0ced4b66c5
Hoang Kiem <kiemhd@outlook.com>
2020-02-15 00:58:43 +0700

    Delete profile and user account

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
+            useFindAndModify: false
         });
         console.log('MongoDB connected...')
     } catch (err) {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -38,14 +38,14 @@ router.post('/', [
             let user = await User.findOne({email});
 
             if (!user) {
-                res.status(400).json({errors: [{msg: 'Invailid Credenntials'}]});
+                res.status(400).json({errors: [{msg: 'Invalid Credentials'}]});
             }
 
             const isMatch = await bcrypt.compare(password, user.password);
             if (isMatch) {
                 return res
                     .status(400)
-                    .json({errors: [{msg: 'Invailid Credenntials'}]})
+                    .json({errors: [{msg: 'Invalid Credentials'}]})
             }
 
             const payload = {
