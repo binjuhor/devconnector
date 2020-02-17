38e5fd9c1f55d4f25f22ed552f11620aefb0ee1d
Hoang Kiem <kiemhd@outlook.com>
2020-02-17 11:32:54 +0700

    Add & Remove education from profile

--- models/Profile.js
+++ models/Profile.js
@@ -55,6 +55,35 @@ const ProfileSchema = new mongoose.Schema({
             }
         }
     ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
     social: {
         youtube: {
             type: String
