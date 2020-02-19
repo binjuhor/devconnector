6bf999c7579399bb3b73e7d74bc05df3ac9681c5
Hoang Kiem <kiemhd@outlook.com>
2020-02-19 11:26:58 +0700

    Post like Unlike Routes [unlike]

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -121,4 +121,29 @@ router.put('/like/:id', auth, async(req, res) => {
    }
 });
 
+
+// @route  PUT api/posts/unlike/:id
+// @desc   Unlike a post
+// @access Private
+router.put('/unlike/:id', auth, async(req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has aready been linked
+        if (0 === post.likes.filter(like => like.user.toString() === req.user.id).length){
+            return res.status(400).json({msg: 'Post has not yet been liked'});
+        }
+        // Get remove index
+        const removeIndex = post.likes.map(like => like.user.toString().indexOf(req.user.id));
+        post.likes.splice(removeIndex,1);
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
