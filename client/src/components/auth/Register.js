5791f4b6649349013a7d1710b765804698e805a1
Hoang Kiem <kiemhd@outlook.com>
2020-03-05 08:28:17 +0700

    Auth Reducer & Register Action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,10 @@ import React, {Fragment, useState} from 'react';
 import {connect} from "react-redux";
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import {setAlert} from "../../actions/alert";
+import {register} from "../../actions/auth";
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -22,14 +22,7 @@ const Register = ({setAlert}) => {
         if (password !== password2) {
             setAlert('Password do not match', 'danger');
         } else {
-            console.log('SUCCESS')
-
-            try {
-
-            } catch (err) {
-                console.error(err.response.data);
-            }
-
+            register({name, email, password})
         }
     }
 
@@ -77,8 +70,11 @@ const Register = ({setAlert}) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 };
 
-
-export default connect(null, {setAlert})(Register);
+export default connect(
+    null,
+    {setAlert, register}
+)(Register);
