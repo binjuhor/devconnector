edc9972bd3de8ffbea82570433e3526d77a56952
Hoang Kiem <kiemhd@outlook.com>
2020-03-05 16:05:02 +0700

    User Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -9,42 +12,54 @@ const Login = () => {
 
     const {email, password} = formData;
 
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+    const onChange = (e) => setFormData({...formData, [e.target.name]: e.target.value})
 
-    const onSubmit = async e => {
+    const onSubmit = async (e)=> {
         e.preventDefault();
-        try{
-
-            console.log('Successful');
-        } catch (err) {
-            console.error(err.response.data);
-        }
+        login(email, password);
     }
 
-    return <Fragment>
-        <h1 className="large text-primary">Sign In</h1>
-        <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)}/>
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    minLength="6"
-                    value={password}
-                    onChange={e => onChange(e)}
-                />
-            </div>
+    // Redirect if logged in
+    if(isAuthenticated) {
+        return <Redirect to={'/dashboard'} />
+    }
 
-            <input type="submit" className="btn btn-primary" value="Login"/>
-        </form>
-        <p className="my-1">
-            Don't have an account? <Link to="/register">Register</Link>
-        </p>
-    </Fragment>
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" value={email}
+                           onChange={e => onChange(e)}/>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+
+                <input type="submit" className="btn btn-primary" value="Login"/>
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </Fragment>
+    );
 }
 
-export default Login
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    auth: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, {Fragment, useState} from 'react';
 import {connect} from "react-redux";
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {setAlert} from "../../actions/alert";
 import {register} from "../../actions/auth";
 
-const Register = ({setAlert, register}) => {
+const Register = ({setAlert, register, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -26,55 +26,67 @@ const Register = ({setAlert, register}) => {
         }
     }
 
-    return <Fragment>
-        <h1 className="large text-primary">Sign Up</h1>
-        <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-                <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required/>
-            </div>
-            <div className="form-group">
-                <input type="email" required placeholder="Email Address" name="email" value={email}
-                       onChange={e => onChange(e)}/>
-                <small className="form-text"
-                >This site uses Gravatar so if you want a profile image, use a
-                    Gravatar email</small
-                >
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    minLength="6"
-                    value={password}
-                    onChange={e => onChange(e)}
-                />
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    name="password2"
-                    minLength="6"
-                    value={password2}
-                    onChange={e => onChange(e)}
-                />
-            </div>
-            <input type="submit" className="btn btn-primary" value="Register"/>
-        </form>
-        <p className="my-1">
-            Already have an account? <Link to="/login">Sign In</Link>
-        </p>
-    </Fragment>
+    if (isAuthenticated) {
+        return <Redirect to={'/dashboard'}/>
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}
+                           required/>
+                </div>
+                <div className="form-group">
+                    <input type="email" required placeholder="Email Address" name="email" value={email}
+                           onChange={e => onChange(e)}/>
+                    <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                        Gravatar email</small
+                    >
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        value={password2}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
-    register: PropTypes.func.isRequired
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
 export default connect(
-    null,
+    mapStateToProps,
     {setAlert, register}
 )(Register);
