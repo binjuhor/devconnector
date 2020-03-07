0e918c7cdff28f1f7df53c87272163b485854309
Hoang Kiem <kiemhd@outlook.com>
2020-03-07 15:47:13 +0700

    Profile Reducer & Get Current Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,7 +12,7 @@ const User = require('../../models/User');
 // @route  GET api/profile/me
 // @desc   Get current users profile
 // @access Private
-router.post('/me', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({user: req.user.id}).populate(
             'user',
