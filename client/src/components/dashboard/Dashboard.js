5bdce0ab07264ebe051d16e05aa38c917e98eca2
Hoang Kiem <kiemhd@outlook.com>
2020-03-10 22:48:18 +0700

    Create Profile Component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
             <i className="fas fa-user"></i> Welcome {user && user.name}
         </p>
         {null !== profile ? (
-            <Fragment>has</Fragment>
+            <Fragment>has profile</Fragment>
         ): (
             <Fragment>
                 <p>You have not yet setup a profile, please add some info</p>
