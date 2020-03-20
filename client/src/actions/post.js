1f91d1a51cb4311e7c8e84b749356b6c92503df2
Hoang Kiem <kiemhd@outlook.com>
2020-03-20 22:35:55 +0700

    Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import {DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, ADD_POST} from './types';
+import {DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES, ADD_POST, GET_POST} from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -92,6 +92,24 @@ export const addPost = formData => async dispatch => {
         });
 
         dispatch(setAlert('Post created', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+// Get post
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`);
+
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        });
+
     } catch (err) {
         dispatch({
             type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST} from '../actions/types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST} from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -18,18 +18,24 @@ export default function (state = initialState, action) {
                 posts: payload,
                 loading: false
             };
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false
+            };
         case DELETE_POST:
             return {
                 ...state,
                 posts: state.posts.filter(post => post._id !== payload ),
                 loading: false
-            }
+            };
         case ADD_POST:
             return {
                 ...state,
                 posts: [payload, ...state.posts],
                 loading: false
-            }
+            };
         case POST_ERROR:
             return {
                 ...state,
