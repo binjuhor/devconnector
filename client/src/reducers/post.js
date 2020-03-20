765a9f727111517a782dbe8cc655cd48196e08ef
Hoang Kiem <kiemhd@outlook.com>
2020-03-20 17:22:33 +0700

    Like & Unlike functionaly

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,7 +1,4 @@
-import {
-    GET_POSTS,
-    POST_ERROR
-} from '../actions/types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES} from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -10,8 +7,8 @@ const initialState = {
     error: {}
 };
 
-export default function(state = initialState, action ){
-    const {type, payload } = action;
+export default function (state = initialState, action) {
+    const {type, payload} = action;
 
     switch (type) {
 
@@ -25,6 +22,13 @@ export default function(state = initialState, action ){
             return {
                 ...state,
                 error: payload,
+                loading: false
+            };
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post =>
+                    post._id === payload.id ? {...post, likes: payload.likes} : post),
                 loading: false
             };
         default:
