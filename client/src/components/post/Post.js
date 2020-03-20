622af088a98f118c6b2a843c92aabf4b1184f8c1
Hoang Kiem <kiemhd@outlook.com>
2020-03-21 00:58:05 +0700

    Update 404 Pages and fix some console.log warning

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -11,11 +11,13 @@ import CommentItem from './CommentItem';
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(() => {
         getPost(match.params.id)
-    }, [getPost]);
+    }, [getPost, match.params.id]);
 
     return loading || post === null ? <Spinner/> : <Fragment>
         <Link to={'/posts'} className={'btn'}>Back To Posts</Link>
         <PostItem post={post} showActions={false}/>
+        <CommentForm postId={post._id} />
+        <CommentForm postId={post._id} />
         <CommentForm postId={post._id} />
         <div className="comments">
             {post.comments.map(comment => (
