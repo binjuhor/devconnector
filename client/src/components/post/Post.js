078b5154636334dca4fb9f9a64871a30ac2e6eb3
Hoang Kiem <kiemhd@outlook.com>
2020-04-08 09:00:26 +0700

    Update gitignore and post, profile

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -17,8 +17,6 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         <Link to={'/posts'} className={'btn'}>Back To Posts</Link>
         <PostItem post={post} showActions={false}/>
         <CommentForm postId={post._id} />
-        <CommentForm postId={post._id} />
-        <CommentForm postId={post._id} />
         <div className="comments">
             {post.comments.map(comment => (
                 <CommentItem key={comment._id} comment={comment} postId={post._id} />
