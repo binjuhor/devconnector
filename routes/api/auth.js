7cce23af223ec3c6d625d2dd8d322f05c1159688
Hoang Kiem <kiemhd@outlook.com>
2025-07-29 11:28:30 +0700

    fix: auth error when have right password

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -42,7 +42,7 @@ router.post('/', [
             }
 
             const isMatch = await bcrypt.compare(password, user.password);
-            if (isMatch) {
+            if (!isMatch) {
                 return res
                     .status(400)
                     .json({errors: [{msg: 'Invalid Credentials'}]})
